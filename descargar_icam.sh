683ccfeda70484a051287a7ee5920e6d7a2f10a5
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-08-29 16:11:54 +0100

    Update descargar_icam.sh

--- descargar_icam.sh
+++ descargar_icam.sh
@@ -1,20 +1,28 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 # Descargar icams inside folder
-#P. Espin
-#file 1 is lista from example "ls /gws/nopw/j04/nceo_geohazards_vol1/public/LiCSAR_products/40/040D_09102_131313/epochs/ -1 >> lista.txt"
+# P. Espin
+
+# file 1 is lista from example:
+# ls /gws/nopw/j04/nceo_geohazards_vol1/public/LiCSAR_products/40/040D_09102_131313/epochs/ -1 >> lista.txt
 
 parent_dir=$(basename "$(dirname "$(pwd)")")
 current_dir=$(basename "$(pwd)")
 
 tr=$(echo $current_dir | cut -d '_' -f1 | sed 's/^0//' | sed 's/^0//' | rev | cut -c 2- | rev)
 
-if [ ! -d "lista.txt" ]; then rm -rf lista.txt fi
+# Make sure lista.txt is fresh
+if [ -f "lista.txt" ]; then
+    rm -f lista.txt
+fi
 
-ls -d /gws/nopw/j04/nceo_geohazards_vol1/public/LiCSAR_products/$tr/$current_dir/epochs/2025* | awk -F'/' '{print $NF}' >> lista.txt
+# Generate list of epochs
+ls -d /gws/nopw/j04/nceo_geohazards_vol1/public/LiCSAR_products/$tr/$current_dir/epochs/2025* \
+    | awk -F'/' '{print $NF}' >> lista.txt
 
-file=lista.txt #$1
+file="lista.txt"
 
+# Loop over epochs
 while read -r line; do
-    echo -e "$line"
-    ./icams_one_epoch.sh $(basename "$(pwd)") $line    
-done <$file
+    echo "$line"
+    ./icams_one_epoch.sh "$current_dir" "$line"
+done < "$file"
